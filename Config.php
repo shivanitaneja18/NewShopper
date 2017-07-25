b726ce44fc296aaaa18b6712304e3095cb5c2424
Shivani Taneja <shivani.taneja104@gmail.com>
2017-07-25 15:15:30 +0530

    Update Config.php

--- Config.php
+++ Config.php
@@ -2,9 +2,9 @@
 <?php
  
  
-define("DB_HOST", "testdb.c0vvfkdln5ew.us-west-2.rds.amazonaws.com");
-define("DB_USER", "root");
-define("DB_PASSWORD", "user123456");
-define("DB_DATABASE", "testdb");
+define("DB_HOST", " ");
+define("DB_USER", " ");
+define("DB_PASSWORD", " ");
+define("DB_DATABASE", " ");
  
 ?>
